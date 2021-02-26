ead8f1e3cb690b6105b0dcae6e0bd02225c1f840
jeresagasti <66226668+jeresagasti@users.noreply.github.com>
2021-02-26 16:35:24 -0300

    Add files via upload

---
+++ inicia.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+$(document).ready(function(){
+   $('.carousel').carousel();
+ });
+
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
